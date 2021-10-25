86ca87986db4a7a5a53825467f09fa44ced666e0
Isaias Abonce <81641405+IsaiasAbonce@users.noreply.github.com>
2021-10-25 14:58:51 -0500

    Add port in MySQLI

--- Database/Database.php
+++ Database/Database.php
@@ -1,9 +1,10 @@
 <?php
-    $host = "localhost:3306";
+    $host = "localhost";
     $user = "root";
     $pass = "root";
     $dbname = "project";
-    $conn = new mysqli($host , $user, $pass, $dbname);
+    $port = "3306";
+    $conn = new mysqli($host , $user, $pass, $dbname, $port);
     mysqli_query($conn , "SET character_set_result=utf8");
     if($conn->connect_error){
         die("Database Error : " . $conn->connect_error);
