64b4c7936240c7ab87c8b3599fd72fc658fc93b2
Isaias Abonce <81641405+IsaiasAbonce@users.noreply.github.com>
2021-10-28 22:38:46 -0500

    Update Database.php

--- Database/Database.php
+++ Database/Database.php
@@ -1,12 +1,12 @@
 <?php
-    $host = "localhost";
+    $host = "db";
     $user = "root";
-    $pass = "root";
+    $pass = "example";
     $dbname = "project";
-    $port = "3306";
-    $conn = new mysqli($host , $user, $pass, $dbname, $port);
+    
+    $conn = new mysqli($host , $user, $pass, $dbname);
     mysqli_query($conn , "SET character_set_result=utf8");
     if($conn->connect_error){
-        die("Database Error : " . $conn->connect_error);
+        die("Database Error : " . $conn->connect_error . $host);
     }
 ?>
